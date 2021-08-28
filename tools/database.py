b86d1c3b8e0f3d29efd1a6afa41457f00fca278e
12eeee1 <73354150+12eeee1@users.noreply.github.com>
2021-08-28 12:31:43 +0800

    Update database.py

--- tools/database.py
+++ tools/database.py
@@ -5,18 +5,19 @@ import numpy as np
 #author = 12eeee
 #sqlite test
 #-----------------#
+#SQL基本操作
 
-#連接資料庫(若名子存在就開啟，否則新建一個資料庫)
+#連接資料庫(若名子存在就開啟開資料庫，否則新建一個資料庫)
 conn = sqlite3.connect("new09.db")
 
 #建立游標(讓Python可以控制資料庫)
 cu = conn.cursor()
 
-#在資料庫中新建Table，欄位自訂
+#在資料庫中新建資料表，命名為data，欄位自訂(node,time,value)。注意資料表名稱不可重複(本例data已經使用過了)
 cu.execute("create table data(node,time,value)")
 
-#在Table更新資料:
-#在node= Node3 的欄位中，更改他的value為121
+#在資料表data更新資料:
+#例:在node= Node3 的欄位中，更改他的value為121
 #cu.execute("update data set node = 'Node3' where value = '121'")
 
 #在Table中建立資料
